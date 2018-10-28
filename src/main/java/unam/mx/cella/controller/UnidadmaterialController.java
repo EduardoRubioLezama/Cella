d4395b213c3f4379ec1bdd8ff0445edabdf900d7
Rossana96 <36312969+Rossana96@users.noreply.github.com>
2018-10-27 19:35:25 -0500

    Agregando mi ramma

---
+++ src/main/java/unam/mx/cella/controller/UnidadmaterialController.java
@@ -0,0 +1,59 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unam.mx.cella.controller;
+
+import java.util.Locale;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManagerFactory;
+import unam.mx.cella.modelo.EntityProvider;
+import unam.mx.cella.modelo.Unidadmaterial;
+import unam.mx.cella.modelo.controller.UnidadmaterialJpaController;
+
+/**
+ *
+ * @author rossa
+ */
+@ManagedBean
+@RequestScoped
+public class UnidadmaterialController {
+
+    /**
+     * Creates a new instance of UnidadmaterialController
+     */
+    private final EntityManagerFactory emf;
+    private Unidadmaterial u_material;
+    public UnidadmaterialController() {
+        emf = EntityProvider.provider();
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(
+                new Locale("es-Mx"));
+    }
+    
+    public Unidadmaterial getUnidadMaterial(){
+        return u_material;
+    }
+    
+    public void setUnidadMaterial(Unidadmaterial u_mat){
+        u_material=u_mat;
+    }
+    
+    public String addUnidadMaterial(){
+        UnidadmaterialJpaController umjpa = new UnidadmaterialJpaController(emf);
+        Unidadmaterial umt = new Unidadmaterial();
+        umt.setNombrematerial(u_material.getNombrematerial());
+        umt.setEstado(u_material.getEstado());
+        umt.setIdMaterial(u_material.getIdMaterial());
+        umjpa.create(u_material);
+    
+        FacesContext.getCurrentInstance().addMessage(null,
+                                                         new FacesMessage(FacesMessage.SEVERITY_INFO,
+                                                                          "Felicidades, el registro se ha realizado correctamente", ""));
+        return null;
+    }
+    
+}
--- target/classes/unam/mx/cella/modelo/Profesor_.java
+++ target/classes/unam/mx/cella/modelo/Profesor_.java
@@ -1,12 +1,12 @@
 package unam.mx.cella.modelo;
 
 import javax.annotation.Generated;
-import javax.persistence.metamodel.ListAttribute;
+import javax.persistence.metamodel.CollectionAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import unam.mx.cella.modelo.Kit;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-22T15:33:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-25T16:55:55")
 @StaticMetamodel(Profesor.class)
 public class Profesor_ { 
 
@@ -18,9 +18,9 @@ public class Profesor_ {
     public static volatile SingularAttribute<Profesor, String> correo;
     public static volatile SingularAttribute<Profesor, String> contrasena;
     public static volatile SingularAttribute<Profesor, String> apellidom;
-    public static volatile ListAttribute<Profesor, Kit> kitList;
     public static volatile SingularAttribute<Profesor, Integer> id;
     public static volatile SingularAttribute<Profesor, String> nombre;
+    public static volatile CollectionAttribute<Profesor, Kit> kitCollection;
     public static volatile SingularAttribute<Profesor, String> rfc;
 
 }
