11c02a74516c4419684805ff421a552ecd4272f1
JaneBlom <jane.blomarz@gmail.com>
2018-11-01 20:26:04 -0600

    Agregando cambios

---
+++ src/main/java/unam/mx/cella/controller/SubcategoriaController.java
@@ -0,0 +1,69 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package unam.mx.cella.controller;
+
+
+import java.util.Locale;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.faces.application.FacesMessage;
+import javax.persistence.EntityManagerFactory;
+import unam.mx.cella.modelo.EntityProvider;
+import unam.mx.cella.modelo.Subcategorias;
+import unam.mx.cella.modelo.controller.SubcategoriasJpaController;
+
+/**
+ *
+ * @author Janeth
+ */
+@ManagedBean
+@RequestScoped
+public class SubcategoriaController {
+
+    private final EntityManagerFactory emf;
+    private Subcategorias subcategoria;
+ 
+    
+    /**
+     * Creates a new instance of SubcategoriaController
+     */
+    public SubcategoriaController() {
+        emf = EntityProvider.provider();
+        System.out.println("creado");
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(
+                new Locale("es-Mx"));
+        this.subcategoria = new Subcategorias();
+        
+    }
+    public Subcategorias getSubcategoria(){
+        return subcategoria;
+    }
+    
+    public void setSubcategoria(Subcategorias nueva){
+        subcategoria = nueva;
+    }
+
+    
+    public String addSubcategoria() {
+       
+            SubcategoriasJpaController sjc = new SubcategoriasJpaController(emf);
+            Subcategorias subcateg = new Subcategorias();
+                subcateg.setNombrecategoria(subcategoria.getNombrecategoria());
+                subcateg.setNombresubcategoria(subcategoria.getNombresubcategoria());
+                subcateg.setIdCategoria(subcategoria.getIdCategoria());
+                sjc.create(subcategoria);
+            
+            
+            FacesContext.getCurrentInstance().addMessage(null,
+                                                         new FacesMessage(FacesMessage.SEVERITY_INFO,
+                                                                          "El registro se ha realizado correctamente", ""));
+         
+        return null;
+    }
+   
+    
+}
--- target/classes/unam/mx/cella/modelo/Subcategorias_.java
+++ target/classes/unam/mx/cella/modelo/Subcategorias_.java
@@ -5,7 +5,7 @@ import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 import unam.mx.cella.modelo.Categoria;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-22T15:33:25")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-10-25T01:42:19")
 @StaticMetamodel(Subcategorias.class)
 public class Subcategorias_ { 
 
