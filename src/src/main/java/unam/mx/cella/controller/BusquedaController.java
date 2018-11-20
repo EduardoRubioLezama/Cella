acd9c895113ab7e55fd0e16f9e4a951d4b78ffde
Rossana96 <36312969+Rossana96@users.noreply.github.com>
2018-11-20 17:01:32 -0600

    Actualización

--- src/src/main/java/unam/mx/cella/controller/BusquedaController.java
+++ src/src/main/java/unam/mx/cella/controller/BusquedaController.java
@@ -1,1 +1,41 @@
+package unam.mx.cella.controller;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.util.Locale;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.context.FacesContext;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import unam.mx.cella.modelo.EntityProvider;
+import unam.mx.cella.modelo.Material;
+import unam.mx.cella.modelo.Unidadmaterial;
+import unam.mx.cella.modelo.MaterialJpaController;
+import unam.mx.cella.modelo.UnidadmaterialJpaController;
+
+/**
+ *
+ * @author rossa
+ */
+@ManagedBean
+@RequestScoped
+public class MaterialController {
+
+    /**
+     * Creates a new instance of MaterialController
+     */
+    
+    private final EntityManagerFactory emf;
+    private Material material;
+    private Unidadmaterial unidadmaterial;
+    private String nombrematerial;
+    private String descripcion;
+    private String estado;
+
+}
