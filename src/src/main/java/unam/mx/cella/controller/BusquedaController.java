158dbbf3f05e88384ecbe66b6de4b9feacc6475a
Rossana96 <36312969+Rossana96@users.noreply.github.com>
2018-11-21 12:28:09 -0600

    Actualizacion

--- src/src/main/java/unam/mx/cella/controller/BusquedaController.java
+++ src/src/main/java/unam/mx/cella/controller/BusquedaController.java
@@ -1,11 +1,15 @@
-package unam.mx.cella.controller;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package unam.mx.cella.controller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import java.util.ArrayList;
+import static java.util.Collections.list;
+import java.util.List;
 import java.util.Locale;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
@@ -16,8 +20,8 @@ import javax.persistence.EntityManagerFactory;
 import unam.mx.cella.modelo.EntityProvider;
 import unam.mx.cella.modelo.Material;
 import unam.mx.cella.modelo.Unidadmaterial;
-import unam.mx.cella.modelo.MaterialJpaController;
-import unam.mx.cella.modelo.UnidadmaterialJpaController;
+import unam.mx.cella.modelo.controller.MaterialJpaController;
+import unam.mx.cella.modelo.controller.UnidadmaterialJpaController;
 
 /**
  *
@@ -30,12 +34,46 @@ public class BusquedaController {
     /**
      * Creates a new instance of BusquedaController
      */
-    
     private final EntityManagerFactory emf;
     private Material material;
-    private Unidadmaterial unidadmaterial;
+    private List<Material> materiales;
     private String nombrematerial;
-    private String descripcion;
-    private String estado;
+    public BusquedaController() {
+        emf = EntityProvider.provider();
+        FacesContext.getCurrentInstance().getViewRoot().setLocale(
+                new Locale("es-Mx"));
+        this.material = new Material();
+        this.materiales = new ArrayList<>();
+        nombrematerial = "";
+    } 
+    
+    public Material getMaterial(){
+        return material;
+    }
+    
+    public void setMaterial(Material material){
+        this.material = material;
+    }
+    
+    public List<Material> getMateriales(){
+        return materiales;
+    }
 
+    public String getNombrematerial(){
+        return nombrematerial;
+    }
+    
+    public void setNombrematerial(String nombrematerial){
+        this.nombrematerial = nombrematerial;
+    }
+    
+    public List<Material> ResultBusqueda(){
+        MaterialJpaController mjpa = new MaterialJpaController(emf);
+        List<Material> mt = mjpa.findMaterials(nombrematerial);
+        return mt;
+    }
+    
+    private EntityManager getEntityManager() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
 }
